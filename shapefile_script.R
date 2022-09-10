c035028a5e846895973984c1fdbbe128e3903997
Andrew Tiu <mt1229@georgetown.edu>
2022-09-09 20:13:42 -0400

    Added county shapefile from R script

--- shapefile_script.R
+++ shapefile_script.R
@@ -1,6 +1,12 @@
+# script for reading in and manipulating US 2020 census county shapefile 
+
+library(tidyverse)
+library(sp)
+
 # read shapefile from Census Bureau -- 2020 version
-county_sp <- sf::st_read('data/cb_2020_us_county_5m/cb_2020_us_county_5m.shp',
-                         quiet=T) %>%
+# downloaded all the files from here: https://www.census.gov/cgi-bin/geo/shapefiles/index.php
+# used web interface to download (year = 2020, layer type = counties/equivalent)
+county_sp <- sf::st_read('cb_2020_us_county_5m/cb_2020_us_county_5m.shp', quiet=T) %>%
   filter(!(STATEFP %in% c('66','69','72','78','60'))) %>% # ignore territories
   select(STUSPS, GEOID, geometry) %>%
   rename(FIPS=GEOID,
@@ -8,8 +14,8 @@ county_sp <- sf::st_read('data/cb_2020_us_county_5m/cb_2020_us_county_5m.shp',
   sf::st_transform(crs=2163) %>%
   as('Spatial') # convert to Spatial obj
 
-# transform function
-# from urbnmapr package source code
+# transform function to handle AK and HI
+# borrowed from urbnmapr package source code
 transform_state <- function(object, rot, scale, shift) {
   object %>%
     maptools::elide(rotate=rot) %>%
@@ -37,4 +43,5 @@ county_sf <- county_sp[!county_sp$state %in% c('AK','HI'),] %>%
 #   geom_sf(data=county_sf, color='black', size=0.05)
 
 # save sf object for later plotting
-sf::st_write(county_sf, 'data/county_sf.shp')
+# 4 output files
+sf::st_write(county_sf, 'US_2020_county_sf.shp')

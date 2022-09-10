29194330cd48e4f8a395bb04e49e831b0ca2ffa2
Andrew Tiu <mt1229@georgetown.edu>
2022-09-10 00:02:06 -0400

    Testing diff projection

--- shapefile_script.R
+++ shapefile_script.R
@@ -11,8 +11,8 @@ county_sp <- sf::st_read('cb_2020_us_county_5m/cb_2020_us_county_5m.shp', quiet=
   select(STUSPS, GEOID, geometry) %>%
   rename(FIPS=GEOID,
          state=STUSPS) %>%
-  sf::st_transform(crs=2163) %>%
-  # sf::st_transform(crs=5070) %>%
+  # sf::st_transform(crs=2163) %>%
+  sf::st_transform(crs=5070) %>%
   as('Spatial') # convert to Spatial obj
 
 # transform function to handle AK and HI -- stick them below continental US
@@ -27,24 +27,24 @@ transform_state <- function(object, rot, scale, shift) {
 
 # version corresponding to crs=2163
 # transform AK
-alaska <- county_sp[county_sp$state=='AK',] %>%
-  transform_state(-35, 2, c(-2600000, -2300000))
-sp::proj4string(alaska) <- proj4string(county_sp)
+# alaska <- county_sp[county_sp$state=='AK',] %>%
+#   transform_state(-35, 2, c(-2600000, -2300000))
+# sp::proj4string(alaska) <- proj4string(county_sp)
 
 # transform HI
-hawaii <- county_sp[county_sp$state=='HI',] %>%
-  transform_state(-35, 0.8, c(-1170000, -2363000))
-sp::proj4string(hawaii) <- proj4string(county_sp)
+# hawaii <- county_sp[county_sp$state=='HI',] %>%
+#   transform_state(-35, 0.8, c(-1170000, -2363000))
+# sp::proj4string(hawaii) <- proj4string(county_sp)
 
 
 # version corresponding to crs=5070
-# alaska <- county_sp[county_sp$state=='AK',] %>%
-#   transform_state(-39, 2, c(-2700000, -100000))
-# sp::proj4string(alaska) <- proj4string(county_sp)
+alaska <- county_sp[county_sp$state=='AK',] %>%
+  transform_state(-39, 2, c(-2700000, -100000))
+sp::proj4string(alaska) <- proj4string(county_sp)
 
-# hawaii <- county_sp[county_sp$state=='HI',] %>%
-#   transform_state(-35, 0.8, c(-900000, -200000))
-# sp::proj4string(hawaii) <- proj4string(county_sp)
+hawaii <- county_sp[county_sp$state=='HI',] %>%
+  transform_state(-35, 0.8, c(-900000, -200000))
+sp::proj4string(hawaii) <- proj4string(county_sp)
 
 # recombine with original shapefile
 county_sf <- county_sp[!county_sp$state %in% c('AK','HI'),] %>%
@@ -53,7 +53,7 @@ county_sf <- county_sp[!county_sp$state %in% c('AK','HI'),] %>%
 
 # save sf object for later plotting
 # 4 output files
-sf::st_write(county_sf, 'US_2020_county_sf.shp')
+sf::st_write(county_sf, 'US_2020_county_shapefile.shp')
 
 # test mapping
 ggplot() +
